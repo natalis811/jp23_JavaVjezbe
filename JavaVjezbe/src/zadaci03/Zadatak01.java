2d23db6413622ef1a028a1912296623e1b26d10d
Natalija Santek <natalis811@gmail.com>
2021-01-20 15:40:53 +0100

    Ispravak jer sam kod prvog rjesenja previdjela rijec "unosi".

--- JavaVjezbe/src/zadaci03/Zadatak01.java
+++ JavaVjezbe/src/zadaci03/Zadatak01.java
@@ -1,16 +1,26 @@
 package zadaci03;
 
+import javax.swing.JOptionPane;
+
 public class Zadatak01 {
 
 	public static void main(String[] args) {
 		// Program unosi brojeve sve dok ne dođe broj -1
-		//Program ispisuje sumu svih unesenih bojeva
-		int i=10;
-		int sum=0;
-		while(i>-1) {
+		//Program ispisuje sumu svih unesenih brojeva
+//		int i=10;
+//		int sum=0;
+//		while(i>-1) {
+//			sum=sum+i;
+//			System.out.println(i);
+//			i--;
+//		}
+//		System.out.println("sum:" + sum);
+		int i = 0;
+		int sum = 0;
+		while (i >-1){
+			String a = JOptionPane.showInputDialog("Unesi broj");
+			      i = Integer.parseInt(a);
 			sum=sum+i;
-			System.out.println(i);
-			i--;
 		}
 		System.out.println("sum:" + sum);
 	}
